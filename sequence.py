18688376c79fdd0f7e86b4db178bca23777131e2
Kyle <kylejosrph@protonmail.com>
2022-11-26 10:45:48 -0400

    new stuff

--- sequence.py
+++ sequence.py
@@ -2,12 +2,17 @@
 import time
 import pyscreenshot
 import numpy as np
+from PIL import Image
 
-# im=pyscreenshot.grab(bbox=(x1,x2,y1,y2))
-image = pyscreenshot.grab(bbox=(10, 10, 500, 500))
+for i in range(3)
+	# im=pyscreenshot.grab(bbox=(x1,x2,y1,y2))
+	screenshot = pyscreenshot.grab(bbox=(10, 10, 500, 500))
 
-# To view the screenshot
-image.show()
+	# To view the screenshot
+	screenshot.show()
 
-# To save the screenshot
-image.save("word.png")
+	# To save the screenshot
+	screenshot.save(f"word{i}.png")
+
+	im = Image.open(sample_path.bmp)
+	p = np.array(im
