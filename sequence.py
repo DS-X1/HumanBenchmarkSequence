3687578d2bdd86efbdb4f5096d29461fa1e6d5f5
Kyle <kylejosrph@protonmail.com>
2022-11-26 10:10:12 -0400

    placeholder

--- sequence.py
+++ sequence.py
@@ -1,6 +1,7 @@
 # Program for partial screenshot
 import time
 import pyscreenshot
+import numpy as np
 
 # im=pyscreenshot.grab(bbox=(x1,x2,y1,y2))
 image = pyscreenshot.grab(bbox=(10, 10, 500, 500))
@@ -9,4 +10,4 @@ image = pyscreenshot.grab(bbox=(10, 10, 500, 500))
 image.show()
 
 # To save the screenshot
-image.save("GeeksforGeeks.png")
+image.save("word.png")
