bc81d3a764e0005ed36423b88d5b8c1ae5f340fb
Kyle <kylejosrph@protonmail.com>
2022-11-26 10:50:08 -0400

    epic

--- sequence.py
+++ sequence.py
@@ -3,8 +3,9 @@ import time
 import pyscreenshot
 import numpy as np
 from PIL import Image
+image_list = []
 
-for i in range(3)
+for i in range(3):
 	# im=pyscreenshot.grab(bbox=(x1,x2,y1,y2))
 	screenshot = pyscreenshot.grab(bbox=(10, 10, 500, 500))
 
@@ -14,5 +15,8 @@ for i in range(3)
 	# To save the screenshot
 	screenshot.save(f"word{i}.png")
 
-	im = Image.open(sample_path.bmp)
-	p = np.array(im
+	im = Image.open(f"word{i}.png")
+	p = np.array(im)
+	image_list.append(p)
+	print(image_list)
+	time.sleep(2)
