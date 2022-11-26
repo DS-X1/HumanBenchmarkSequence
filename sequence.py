0f14b80bbe659ea5513388d244040462ee2c5b3a
Kyle <kylejosrph@protonmail.com>
2022-11-26 11:19:21 -0400

    1.0

--- sequence.py
+++ sequence.py
@@ -3,6 +3,8 @@ import time
 import pyscreenshot
 import numpy as np
 from PIL import Image
+import mouse
+
 image_list = []
 
 #seen x=606, y=504
@@ -24,7 +26,11 @@ for i in range(3):
 
 	if p in image_list:
 		print("seen image")
+		mouse.move(606,504,absolute=True,duration=0)
+		mouse.click(button='left')
 		pass
 	elif p not in image_list:
 		image_list.append(p)
+		mouse.move(744,504,absolute=True,duration=0)
+		mouse.click(button='left')
 		print("new image")
